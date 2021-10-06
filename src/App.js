bb6bc9fdb9d6eaf2db4b8bcf0dd6e833c8f2bf87
Dave Madden <davelearnsstuff@gmail.com>
2021-10-06 12:26:32 -0700

    that was fast

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+// import { BASE_URL, API_KEY } from "./constants";
 import "./App.css";
 
 function App() {
+  // console.log(BASE_URL, API_KEY);
+
+  //state
+  const [pod, setPod] = useState("");
+
+  //use effect
+  //api call
+  useEffect(() => {
+    axios.get("https://api.nasa.gov/planetary/apod?api_key=Sd4BnSA8fJaXBOdPBsZNHlhipGXyDtHharhOlUoK")
+      .then(res => {
+        console.log(res.data);
+        setPod(res.data);
+      }).catch(err => {
+        console.log(err);
+      })
+  }, []);
+  //react render
+
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <h1>{pod.title}</h1>
+      <img src={pod.url} />
+      <p>{pod.explanation}</p>
     </div>
   );
 }
