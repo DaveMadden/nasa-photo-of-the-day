3b5a7dbb3f037f82ecc14f3df93aa0c6d84d9757
Dave Madden <davelearnsstuff@gmail.com>
2021-10-07 11:55:21 -0700

    got it working, just working on adding styles now

--- src/Pod.js
+++ src/Pod.js
@@ -1,15 +1,31 @@
 import React from "react";
+import styled from 'styled-components';
 // console.log("in pod.js")
+
+const StyledPod = styled.div`
+    width: 75%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    border-radius: 5px;
+    border: 2px solid black;
+    background-color: ${pr => pr.theme.darkgrey};
+    color: ${pr => pr.theme.white}
+    p {
+        margin: 20px;
+        padding: 10px;
+    }
+`
 
 const Pod = (props) => {
     const { title, url, explanation } = props.pod;
     
     return(
-        <>
+        <StyledPod>
             <h1>{title}</h1>
             <img src={url} alt={`${explanation}`} />
             <p>{explanation}</p>
-        </>
+        </StyledPod>
     );
 };
 
---
+++ src/theme/index.js
@@ -0,0 +1,10 @@
+// theme object here
+export default {
+    primaryColor: 'royalBlue',
+    secondaryColor: 'crimson',
+    tertiaryColor: 'mediumseagreen',
+    black: 'black',
+    white: 'white',
+    darkgrey: '#343a40',
+    breakpointMobile: '(max-width: 550px)'
+}
